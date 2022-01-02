f0a56421116e42a52250c0fe1b28bb15d3639785
Thoại Nguyễn <you@example.com>
2022-01-02 20:56:57 +0700

    Fix bug chua dang nhap ko the xem danh gia, h da co the xem nhung ko the danh gia

--- mvc/controllers/Ajax.php
+++ mvc/controllers/Ajax.php
@@ -119,7 +119,90 @@
     
                 }
             }else{
-                echo 'Hãy Đăng Nhập Để Đánh Giá';
+    
+                if(isset($_POST["action"]))
+                {
+                    $id_product = $_POST["id_product"];
+                    $average_rating = 0;
+                    $total_review = 0;
+                    $five_star_review = 0;
+                    $four_star_review = 0;
+                    $three_star_review = 0;
+                    $two_star_review = 0;
+                    $one_star_review = 0;
+                    $total_user_rating = 0;
+                    $review_content = array();
+    
+                    $query = "SELECT * FROM review
+                    WHERE id_product = $id_product
+                    ORDER BY review_id DESC
+                    ";
+    
+                    $Result = mysqli_query($this->ReviewModel->conn, $query);
+    
+                    while($row = mysqli_fetch_array($Result, 1)){
+                        
+                        $data[] = $row;
+                       
+                    }
+    
+                    foreach($data as $row)
+                    {
+                        $review_content[] = array(
+                            'user_name'		=>	$row["user_name"],
+                            'user_review'	=>	$row["user_review"],
+                            'rating'		=>	$row["user_rating"],
+                            'datetime'		=>	date('l jS, F Y h:i:s A', $row["datetime"])
+                        );
+    
+                        if($row["user_rating"] == '5')
+                        {
+                            $five_star_review++;
+                        }
+    
+                        if($row["user_rating"] == '4')
+                        {
+                            $four_star_review++;
+                        }
+    
+                        if($row["user_rating"] == '3')
+                        {
+                            $three_star_review++;
+                        }
+    
+                        if($row["user_rating"] == '2')
+                        {
+                            $two_star_review++;
+                        }
+    
+                        if($row["user_rating"] == '1')
+                        {
+                            $one_star_review++;
+                        }
+    
+                        $total_review++;
+    
+                        $total_user_rating = $total_user_rating + $row["user_rating"];
+    
+                    }
+    
+                    $average_rating = $total_user_rating / $total_review;
+    
+                    $output = array(
+                        'average_rating'	=>	number_format($average_rating, 1),
+                        'total_review'		=>	$total_review,
+                        'five_star_review'	=>	$five_star_review,
+                        'four_star_review'	=>	$four_star_review,
+                        'three_star_review'	=>	$three_star_review,
+                        'two_star_review'	=>	$two_star_review,
+                        'one_star_review'	=>	$one_star_review,
+                        'review_data'		=>	$review_content
+                    );
+    
+                    echo json_encode($output);
+    
+                }
+                
             }
         }
 
